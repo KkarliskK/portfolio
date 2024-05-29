c8a1e065425ffd39c9389d14152c7c2f780bd3fb
KkarliskK <42585445+KkarliskK@users.noreply.github.com>
2024-05-29 19:15:04 +0300

    landing page + header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import logo from '../assets/logo.png';
+import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react";
 
 function Header() {
     const [darkTheme, setDarkTheme] = useState(false);
@@ -14,18 +16,29 @@ function Header() {
 
   return (
     <>
-        <div className={`flex w-full h-1/5 `}> {/****This is the whole header****/}
-            <div className={`flex justify-center items-center`}> {/***This is a single element for header*****/}
-                <button className={`p-1 m-1 border-2 border-black rounded font-semibold`} onClick={change_theme}>{darkTheme ? 'Go light' : 'Go dark'}</button>
+        <div className={`flex bg-black w-full h-16 shadow-lg justify-between `}> {/****This is the whole header****/}
+          <div className={``}> <img className={`rounded-full h-full ml-40`} src={logo} /> </div>{/****Logo****/}
+            <div className={`flex `}> {/****Button container*******/}
+              <div className={`flex justify-center items-center`}> {/***This is a single element for header*****/}
+                  <button className={`p-1 m-1 border-2 border-black rounded font-normal text-white`} onClick={change_theme}>{darkTheme ? 'Go light' : 'Go dark'}</button>
+              </div>
+              <div className={`flex justify-center items-center`}>
+                  <button className={`p-1 m-1 border-2 border-black rounded font-normal text-white`}>About Me</button>
+              </div>
+              <div className={`flex justify-center items-center`}>
+                  <button className={`p-1 m-1 border-2 border-black rounded font-normal text-white`}>My Projects</button>
+              </div>
+              <div className={`flex justify-center items-center`}>
+                  <button className={`p-1 m-1 border-2 border-black rounded font-normal text-white`}>Contact Me</button>
+              </div>
             </div>
-            <div>
-                <button className={`p-1 m-1 border-2 border-black rounded font-semibold`}>About Me</button>
-            </div>
-            <div>
-                <button className={`p-1 m-1 border-2 border-black rounded font-semibold`}>My Projects</button>
-            </div>
-            <div>
-                <button className={`p-1 m-1 border-2 border-black rounded font-semibold`}>Contact Me</button>
+            <div className={`flex mr-36`}> {/****Social media container****/}
+              <div className={`flex justify-center items-center`}>
+                <a className={`m-3`} href='https://www.facebook.com/profile.php?id=100008563723697'><FacebookLogo size={38} color="#f7f7f7" /></a>
+                <a className={`m-3`} href='https://www.linkedin.com/in/k%C4%81rlis-birkavs-83301a26b/'><LinkedinLogo size={38} color="#f7f7f7" /></a>
+                <a className={`m-3`} href='https://www.instagram.com/kkarliskk/'><InstagramLogo size={38} color="#f7f7f7" /></a>
+                <a className={`m-3`} href='https://github.com/KkarliskK/'><GithubLogo size={38} color="#f7f7f7" /></a>
+              </div>
             </div>
         </div>
     </>
--- frontend/src/view/LandingPage.jsx
+++ frontend/src/view/LandingPage.jsx
@@ -1,10 +1,21 @@
-import '../style/LandingPage.module.css'
+import { CaretDoubleDown } from '@phosphor-icons/react';
+import css from '../style/LandingPage.module.css'
 
 function LandingPage() {
   return (
     <>
-        <div className={`flex justify-center items-center w-full`}>
-            <h1 className={`flex text-3xl font-semibold`}>Welcome to Landing Page!</h1>
+        <div className={`flex flex-row justify-center items-center w-full h-4/5 shadow-lg`}>
+            <h1 className={`text-3xl font-semibold`}>Kārlis Birkavs</h1>
+            <p>Student at VTDT. </p>
+            <div className={`flex justify-center items-center w-full`}>{/******My latest projects*******/}
+              <h1>My latest projects</h1>
+
+            </div>
+        </div>
+        <div className={`flex w-full justify-center items-center h-1/6`}> {/****About Me small section***/}
+          <div className={`${css.wave_animation}`}><CaretDoubleDown size={32} /></div>
+            <button className={`bg-inherit border-0 m-5 p-2`}><h2 className={`text-2xl font-semibold`}>About me</h2></button>
+          <div className={`${css.wave_animation}`}><CaretDoubleDown size={32} /></div>
         </div>
       
     </>
