cfb4370231a61b84052816ec92f128c6cb249eb0
KkarliskK <42585445+KkarliskK@users.noreply.github.com>
2024-05-30 20:27:59 +0300

    small update on landing page

--- frontend/src/view/LandingPage.jsx
+++ frontend/src/view/LandingPage.jsx
@@ -1,6 +1,9 @@
 import { CaretDoubleDown, CaretRight } from '@phosphor-icons/react';
 import css from '../style/LandingPage.module.css'
 import Font, { Text } from 'react-font'
+import { FaCss3Alt, FaHtml5, FaLaravel, FaReact } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io";
+
 
 function LandingPage() {
   return (
@@ -8,11 +11,18 @@ function LandingPage() {
         <div className={`flex flex-col justify-center items-center w-full h-4/5 shadow-lg `}>
           <div className={`${css.circle}`}></div>
           <div className={`flex w-full`}>
-            <div className={`flex w-2/6 ml-20 p-4`}>
+            <div className={`flex w-2/6 ml-20 p-4 flex-col`}>
               <Font family='Oswald'><h1 className={`text-9xl font-semibold`}>Kārlis Birkavs</h1> </Font>
+              <div className={`flex items-center `}>
+                <p className={`text-4xl m-2`}> <FaHtml5 /></p>
+                <p className={`text-4xl m-2`}> <FaCss3Alt /></p>
+                <p className={`text-4xl m-2`}> <FaLaravel /></p>
+                <p className={`text-4xl m-2`}> <FaReact /></p>
+                <p className={`text-4xl m-2`}> <IoLogoJavascript /></p>
+              </div>
             </div>
             <div className={`flex justify-center w-2/6 flex-col`}>
-              <div className={`flex items-center`}><CaretRight size={28} /><Font family='Oswald'><p className={`text-xl`}>Student at VTDT </p></Font></div>
+              <div className={`flex items-center`}><CaretRight size={28} /><Font family='Oswald'><p className={`text-xl`}>Student at Vidzemes Tehnoloģiju un Dizaina Tehnikums </p></Font></div>
               <div className={`flex items-center`}><CaretRight size={28} /><Font family='Oswald'><p className={`text-xl`}>Front end developer </p></Font></div>
             </div>
             <div className={`flex w-2/6 flex-col`}> {/****Show my latest projects*****/}
